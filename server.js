9fe99c8476c5ea3b75d429a359c3b31d55c65385
jmcdlungren <j.mcd.lungren@gmail.com>
2023-03-09 16:02:27 -0700

    set up beginning of server for HTML to read, working on functionality

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+
+    // Logs the request to the terminal
+    console.info(`${req.method} request received to get notes`);
+});
+
+app.post('/notes', (req, res) => {
+    console.info(`${req.method} request received to add a review`);
+
+    const { title, text } = req.body;
+
+    // if (title && text) {
+    //     const newNote = {
+    //         title,
+    //         text,
+    //     };
+
+    // fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    //     if (err) {
+    //         console.error(err);
+    //     } else {
+    //         const parsedNotes = JSON.parse(data);
+
+    //         parsedNotes.push(newNote);
+
+    //         fs.writeFile(
+    //             './db/db.json',
+    //             JSON.stringify(parsedNotes, null, 4),
+    //             (writeErr) =>
+    //                 writeErr
+    //                 ? console.error(writeErr)
+    //                 : console.info('Successfully updated notes!')
+    //         )
+    //     }
+    // });
+
+    // const response = {
+    //     status: 'success',
+    //     body: newReview,
+    // };
+
+    // console.log(response);
+    // res.status(201).json(response);
+    // } else {
+    //     res.status(500).json('Error in posting note');
+    // }
+})
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
